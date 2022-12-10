9a9b3e51b8f0af432f72cc1401cd5e9989021f1f
Navinn Ravindaran <navinn@duck.com>
2022-12-09 20:20:55 -0500

    feat: setup firebase sdk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,32 @@
 import { Container, Text } from '@chakra-ui/react';
+import { db } from './firebaseConfig';
+import { useList } from 'react-firebase-hooks/database';
+import { DataSnapshot, ref } from 'firebase/database';
+import React from 'react';
+
+const query = ref(db, 'calls');
+
+const CallsTable: React.FC<{ data: DataSnapshot[] }> = ({ data }) => {
+  return (
+    <>
+      {data.map((v) => (
+        <Container key={v.key}>
+          <Text>{v.key}</Text>
+          <Text>{JSON.stringify(v.val(), null, 2)}</Text>
+        </Container>
+      ))}
+    </>
+  );
+};
 
 function App() {
+  const [snapshots, loading, error] = useList(query);
+
   return (
     <Container>
       <Text fontSize="2xl">Assembly AI</Text>
+      {error ? <Text>Error: {error.message}</Text> : null}
+      {!loading && snapshots ? <CallsTable data={snapshots} /> : null}
     </Container>
   );
 }
