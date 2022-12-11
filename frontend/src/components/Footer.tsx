affd5d32c4842a31a458b2109344f871251931d8
Lirak Haxhikadrija <lirak.haxhikadrijawork@gmail.com>
2022-12-11 15:10:40 +0100

    Update Footer logos

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { Badge, Box, chakra, Container, Stack, Text, useColorModeValue, VisuallyHidden } from '@chakra-ui/react';
-import { CgWebsite } from 'react-icons/cg';
 import { FaGithub } from 'react-icons/fa';
 import { ReactNode } from 'react';
+import { BsGlobe } from 'react-icons/bs';
 
 const SocialButton = ({ children, label, href }: { children: ReactNode; label: string; href: string }) => {
   return (
@@ -50,16 +50,16 @@ export function Footer() {
         <Text>Project Submission to AssemblyAI $50K AI Hackathon</Text>
         <Stack direction={'row'} spacing={6}>
           <SocialButton label={'OperatorAI'} href={'https://www.operatorai.tech/'}>
-            <CgWebsite />
+            <BsGlobe />
           </SocialButton>
           <SocialButton label={'Github'} href={'https://github.com/dtemir/assembly-ai-hackathon'}>
             <FaGithub />
           </SocialButton>
-          <SocialButton label={'Instagram'} href={'https://devpost.com/software/draft-w86vkc'}>
-            <img
-              alt="Devpost"
-              src="https://q9k6x7m8.stackpathcdn.com/assets/reimagine2/devpost-logo-646bdf6ac6663230947a952f8d354cad.svg"
-            />
+          <SocialButton label={'Devpost'} href={'https://devpost.com/software/draft-w86vkc'}>
+            <svg role="img" viewBox="-6 -6 36 36" xmlns="http://www.w3.org/2000/svg">
+              <title>Devpost</title>
+              <path d="M6.002 1.61L0 12.004 6.002 22.39h11.996L24 12.004 17.998 1.61zm1.593 4.084h3.947c3.605 0 6.276 1.695 6.276 6.31 0 4.436-3.21 6.302-6.456 6.302H7.595zm2.517 2.449v7.714h1.241c2.646 0 3.862-1.55 3.862-3.861.009-2.569-1.096-3.853-3.767-3.853Z" />
+            </svg>
           </SocialButton>
         </Stack>
       </Container>
